335b71a04808532e0185f7c927aa4b8549630d05
Aleksey Osipov <oad2509@mail.ru>
2024-01-12 18:13:53 +0300

    Актуальный год в меню

--- js/scripts.js
+++ js/scripts.js
@@ -1348,6 +1348,12 @@ $(document).ready(function() {
 
 document.addEventListener("DOMContentLoaded", (event) => {
     const scrollDown = document.getElementById('scroll-down')
+    const navFooter = document.getElementsByClassName('nav-footer');
+    const year = navFooter[0].parentNode.getElementsByTagName('span')[0];
+
+    const currentYear = new Date().getFullYear();
+
+    year.innerHTML = year.innerHTML.replace('2022', currentYear)
 
     scrollDown.addEventListener('click',() => {
         window.scrollBy({
